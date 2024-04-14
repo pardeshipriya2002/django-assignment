390966bc7563a03a9194bf19b85775c97eaa44b3
pardeshipriya2002 <161711481+pardeshipriya2002@users.noreply.github.com>
2024-04-14 16:12:36 +0530

    Delete TaskManager directory

--- TaskManager/TaskManagerApp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import *
-# Register your models here.
-
-class Student(admin.ModelAdmin):
-    list_display=['Name']
-admin.site.register(User,Student)
-admin.site.register(Task)
--- TaskManager/TaskManagerApp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from django.shortcuts import render
-from .views import *
-
-urlpatterns = [
-    path('',home,name='index'),
-    path('register',registration,name='register'),
-]
